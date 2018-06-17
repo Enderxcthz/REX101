64292a3b7729f6d9bd0abcac5b9264661ae4c2a3
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-06-17 15:05:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,14 +10,14 @@ async def on_ready():
   print("REX_101 is now online, and ready to wreck havoc. >:D")
 
 @bot.command()
-async def introduction(ctx, param):
-    await ctx.send("RAWR! BEEP-BOOP-BEEP-BOOP. Hello there human! I am REX_101! The security and interigations bot for this server! :sungalsses:".format(param))
+async def introduction(ctx):
+    await ctx.send("RAWR! BEEP-BOOP-BEEP-BOOP. Hello there human! I am REX_101! The security and interigations bot for this server! :sungalsses:")
     await ctx.send("I hope we can get along nicely :smiley:")
-    await ctx.send(":thinking: **What am I?** I am the bot from the sole-purpose of the Roblox gaming company Instinct Survivial™!".format(param))
-    await ctx.send(":scream: **Can I be in your server?** No, not at the moment, Uncle Endy says I am in Early-stage development, which means...".format(param))
-    await ctx.send(":warning: **COMPUTING...** ".format(param))
-    await ctx.send("Nope! Not just yet c;".format(param))
-    await ctx.send("Well I hope that has answered some of your non-dino brain questions! If you require further assitónce with my commands, a list of them, or further FAQ's, run \"instinct!help\":sparkles: :tada: ".format(param))
-    await ctx.send("Most importantly, always remember: **Survival is key.** :grinning:".format(param))
+    await ctx.send(":thinking: **What am I?** I am the bot from the sole-purpose of the Roblox gaming company Instinct Survivial™!")
+    await ctx.send(":scream: **Can I be in your server?** No, not at the moment, Uncle Endy says I am in Early-stage development, which means...")
+    await ctx.send(":warning: **COMPUTING...** ")
+    await ctx.send("Nope! Not just yet c;")
+    await ctx.send("Well I hope that has answered some of your non-dino brain questions! If you require further assitónce with my commands, a list of them, or further FAQ's, run \"instinct!help\":sparkles: :tada:")
+    await ctx.send("Most importantly, always remember: **Survival is key.** :grinning:")
 
 bot.run(os.environ['TOKEN'])
