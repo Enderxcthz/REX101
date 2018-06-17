c9d6eef82029108a5e30d1f9545c958d74cf4489
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-06-17 13:28:38 +0100

    Create REX101.py

---
+++ REX101.py
@@ -0,0 +1,6 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="instinct!", description="Survival Is Key.")
+
+bot.run("NDU3ODc3NzA5NjMzMDkzNjQy.DgfffQ.LzS-1AXhInrdJj9upl6-xjQ3kaA")
