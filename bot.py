a71b984deccc79392a6a94f35ae7d09c00e0256b
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-06-17 14:13:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,4 +3,4 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="instinct!", description="Survival Is Key.")
 
-bot.run("NDU3ODc3NzA5NjMzMDkzNjQy.DgfffQ.LzS-1AXhInrdJj9upl6-xjQ3kaA")
+bot.run("TOKEN")
