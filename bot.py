070242e5c3089ba1dc481a3d3272076f6051adde
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-06-21 15:51:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,22 @@ bot = commands.Bot(command_prefix="instinct!", description="Survival Is Key.")
 @bot.event
 async def on_ready():
   print("REX_101 is now online, and ready to wreck havoc. >:D")
+  
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('Good'):
+        msg = 'Yes, good indeed, THE TEST SUCEEDED! :tada: {0.author.mention}'.format(message)
+        await bot.send_message(message.channel, msg)
+        
+        
 
 @bot.command()
 async def introduction(ctx):
-    await ctx.send("RAWR! BEEP-BOOP-BEEP-BOOP. Hello there human! I am REX_101! The security and interigations bot for this server! :sungalsses:")
+    await ctx.send("RAWR! BEEP-BOOP-BEEP-BOOP. Hello there human! I am REX_101! The security and interigations bot for this server! :sunglasses:")
     await ctx.send("I hope we can get along nicely :smiley:")
     await ctx.send(":thinking: **What am I?** I am the bot from the sole-purpose of the Roblox gaming company Instinct Survivial™!")
     await ctx.send(":scream: **Can I be in your server?** No, not at the moment, Uncle Endy says I am in Early-stage development, which means...")
@@ -19,5 +31,7 @@ async def introduction(ctx):
     await ctx.send("Nope! Not just yet c;")
     await ctx.send("Well I hope that has answered some of your non-dino brain questions! If you require further assitónce with my commands, a list of them, or further FAQ's, run \"instinct!help\":sparkles: :tada:")
     await ctx.send("Most importantly, always remember: **Survival is key.** :grinning:")
+    
+
 
 bot.run(os.environ['TOKEN'])
